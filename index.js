a9252d5265bcfaa9fd5ff5bb774d7780e0dd03d7
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-05-30 18:11:12 -0300

    Corrections to navbar

--- index.js
+++ index.js
@@ -13,11 +13,25 @@ window.onload = (() => {
 
         let navbar = document.getElementById('navbar');
 
-        if (window.scrollY > 800) {
+        if (window.scrollY > 600) {
             navbar.classList.add("fixed-bottom");
         } else {
             navbar.classList.remove("fixed-bottom");
         }
     });
 
+    $(function () {
+        var navMain = $(".navbar-collapse"); // avoid dependency on #id
+        // "a:not([data-toggle])" - to avoid issues caused
+        // when you have dropdown inside navbar
+        navMain.on("click", "a:not([data-toggle])", null, function () {
+            navMain.collapse('hide');
+        });
+    });
+
 });
+
+
+
+
+
